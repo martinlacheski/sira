c5d566de9fcc0c0d9a1a4319573978775766f534
martinlacheski <martinlacheski@gmail.com>
2020-10-01 10:36:54 -0300

    Commit 01-10-2020 Cambios en el template, usuarios, y con las solicitudes (tipos y motivos) - parche para localidades con una funcion js submit_whitout_ajax

--- app/static/js/functions.js
+++ app/static/js/functions.js
@@ -61,3 +61,52 @@ function submit_with_ajax(url, title, content, parameters, callback) {
         }
     })
 }
+
+//para el select anidado hasta que encuentre como resolver eso
+function submit_without_ajax(url, title, content, parameters, callback) {
+    $.confirm({
+        theme: 'material',
+        title: title,
+        icon: 'fa fa-info',
+        content: content,
+        columnClass: 'small',
+        typeAnimated: true,
+        cancelButtonClass: 'btn-primary',
+        draggable: true,
+        dragWindowBorder: false,
+        buttons: {
+            info: {
+                text: "Si",
+                btnClass: 'btn-primary',
+                action: function () {
+                    $.ajax({
+                        url: url, //window.location.pathname
+                        type: 'POST',
+                        data: parameters,
+                        dataType: 'json',
+                    }).done(function (data) {
+                        console.log(data);
+                        if (!data.hasOwnProperty('error')) {
+                            callback();
+                            return false;
+                        }
+                        message_error(data.error);
+                    }).fail(function (jqXHR, textStatus, errorThrown) {
+                         //alert(textStatus + ': ' + errorThrown);
+                        callback();
+                        return false;
+                    }).always(function (data) {
+
+                    });
+                }
+            },
+            danger: {
+                text: "No",
+                btnClass: 'btn-red',
+                action: function () {
+
+                }
+            },
+        }
+    })
+}
