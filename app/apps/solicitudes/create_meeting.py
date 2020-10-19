4b344bb7e0033c2d8a5f701270b18778010872f0
martinlacheski <martinlacheski@gmail.com>
2020-10-19 17:05:51 -0300

    commit con meetings_api al 19/10/2020

--- app/apps/solicitudes/create_meeting.py
+++ app/apps/solicitudes/create_meeting.py
@@ -38,7 +38,6 @@ def inicio_reunion(data):
 	return(inicio_reunion_formateada)
 
 
-#	"2020-09-17T21:40:00Z",
 
 def fin_reunion(data):
 	if data.is_valid():
@@ -53,7 +52,26 @@ def fin_reunion(data):
 		#print(fin_reunion_formateada)
 	
 	return(fin_reunion_formateada)
+	
+def email_creador(data):
+	if data.is_valid():
+		email = data.cleaned_data.get("email")
+		
+		
+	return(email)
 
+def nombre_creador_reunion(data):
+	if data.is_valid():
+		nombre = data.cleaned_data.get("nombres")
+		apellido = data.cleaned_data.get("apellido")
+		
+		nombre = str(nombre)
+		apellido = str(apellido)
+		
+		nombre_completo = nombre + ' ' + apellido
+	return(nombre_completo)	
+	
+	
 #nos da la respuesta del POST request
 def jprint(obj):
 	text = json.dumps(obj, sort_keys=True, indent=2)
@@ -62,12 +80,12 @@ def jprint(obj):
 
 def crearReunion(form):
 	#Funciones que hacen el trabajo sucio
-	nombre_reunion(form)
-	inicio_reunion(form)
-	fin_reunion(form)	
+	#nombre_reunion(form)
+	#inicio_reunion(form)
+	#fin_reunion(form)	
 	
 	url = "https://webexapis.com/v1/meetings"
-	token = "Bearer NjU1ZDA1NDgtOWFiZC00ZmIzLWE2ODUtNTgzMDE4ZmUzMDY0NDczZGIzZTAtYzRl_P0A1_bbbd8451-22ce-44ba-a050-4b19c5cf4ecb"
+	token = "Bearer MTM5MDg2ZjktN2RlOC00MjA0LWFkOWYtOTA2M2M0NjYyNTI3MDViOTAxZDYtNDQ4_P0A1_bbbd8451-22ce-44ba-a050-4b19c5cf4ecb"
 	
 	headers = {
 
@@ -80,18 +98,33 @@ def crearReunion(form):
 		"allowAnyUserToBeCoHost": true,
 		"title" : nombre_reunion(form),
 		"password" : "202020",
-		"start" : str(inicio_reunion(form)),
+		"start" : inicio_reunion(form),
 		"end" : fin_reunion(form),
 		"timezone": "America/Argentina/Cordoba",
-		"invitees" : []
+		"invitees" : [
+			{
+				"email": str(email_creador(form)),
+				"displayName": str(nombre_creador_reunion(form)),
+				"coHost": false
+			}
+		]
 	}
 	
 	#realiza la request
 	response = requests.post(url, headers=headers, json=datos_reunion)
 	
-	#imprime los datos de vuelta de la request
+	#imprime los datos de vuelta de la request (IMPORTANTE)
 	jprint(response.json())
 	
 	return(0)
 	
-
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
